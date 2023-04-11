f61c1f85c44c02213025b865df41baeb9aea048d
vidi123 <fadil.arofah79@gmail.com>
2023-04-11 10:49:57 +0700

    setup daisy ui in tailwin

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,22 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#645CAA",
+          "secondary": "#CED7F6",
+          "accent": "#1FB2A5",
+          "neutral": "#191D24",
+         "base-100": "#DEDBDB",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
       },
-    },
+    ],
   },
-  plugins: [],
 }
