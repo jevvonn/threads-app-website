81ce7ea5719637b27f997c1f3344a95355893617
vidi123 <fadil.arofah79@gmail.com>
2023-04-11 15:53:20 +0700

    create sign in page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   plugins: [require("daisyui")],
   daisyui: {
     themes: [
       {
         mytheme: {
-          "primary": "#645CAA",
-          "secondary": "#CED7F6",
-          "accent": "#1FB2A5",
-          "neutral": "#191D24",
-         "base-100": "#DEDBDB",
-          "info": "#3ABFF8",
-          "success": "#36D399",
-          "warning": "#FBBD23",
-          "error": "#F87272",
+          primary: "#645CAA",
+          secondary: "#CED7F6",
+          accent: "#1FB2A5",
+          neutral: "#191D24",
+          "base-100": "#DEDBDB",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
         },
       },
     ],
   },
-}
+};
