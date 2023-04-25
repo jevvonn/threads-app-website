d08ddc943f9448d0268e9ac3ef4777791b3ab8a9
DiandraRifqiM <LuxamSky@gmail.com>
2023-04-25 23:10:08 +0700

    nextauth update

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,10 +1,13 @@
-import NextAuth from "next-auth";
+import NextAuth, { getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import TwitterProvider from "next-auth/providers/twitter";
 
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "../../../../prisma/prisma";
 
+/**
+ * @type {import("next-auth").NextAuthOptions}
+ */
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
@@ -19,17 +22,38 @@ export const authOptions = {
     }),
   ],
   callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
+    async session({ session, user, trigger, newSession }) {
+      if (trigger == "update" && newSession) {
+        await prisma.user.update({
+          where: {
+            id: user.id,
+          },
+          data: {
+            name: newSession.name ?? "",
+            image: newSession.image ?? "",
+            bio: newSession.bio ?? "",
+          },
+        });
       }
-      return session;
+
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: user.id,
+          bio: user.bio,
+        },
+      };
     },
   },
   pages: {
     signIn: "/login",
     newUser: "/getting-started",
   },
+};
+
+export const getServerAuthSession = (req, res) => {
+  return getServerSession(req, res, authOptions);
 };
 
 export default NextAuth(authOptions);
