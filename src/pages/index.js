3551b09e9d75d73f5bae7604cbaeecce8a7000b1
jevvonn <darmawanb985@gmail.com>
2023-05-02 21:45:42 +0700

    change and apply component + adding dummy data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,97 @@
-import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
-import { AiOutlineComment } from "react-icons/ai";
-import { AiOutlineHeart } from "react-icons/ai";
 import { AiOutlineSearch } from "react-icons/ai";
 import { AiOutlineUsergroupAdd } from "react-icons/ai";
-import { BsBookmark } from "react-icons/bs";
 import { BsFire } from "react-icons/bs";
 import { BsBarChartFill } from "react-icons/bs";
-import { BiUpvote } from "react-icons/bi";
-import { BiDownvote } from "react-icons/bi";
 import { MdNewReleases } from "react-icons/md";
+import UserDropdown from "@/components/navigation/UserDropdown";
+import SingleLogo from "@/components/logo/SingleLogo";
+import FullLogo from "@/components/logo/FullLogo";
+import SingleThread from "@/components/thread/SingleThread";
+
+const data = [
+  {
+    id: "c40a8157-4878-4f2d-a271-85f10e684ca0",
+    userId: "clh6b3v450000vng0img2thev",
+    type: "POST_BODY",
+    title: "Cerpen Setelah Beliin Anak Mobil Baru",
+    body: `Sebelumnya, Mantan pejabat Direktorat Jenderal Pajak Kementerian                 Keuangan (DJP Kemenkeu) Rafael Alun Trisambodo ditahan KPK. Dia                 diduga menerima USD 90 ribu atau sekitar Rp 1,3 miliar melalui                 perusahaan konsultan pajak miliknya. <br></br>Ketua KPK Firli Bahuri mengatakan, kasus ini bermula                 saat Rafael diangkat menjadi Kepala Bidang Pemeriksaan,                 Penyidikan, dan Penagihan Pajak pada Kantor Wilayah Ditjen Pajak                 Jawa Timur I pada 2011.                  <br></br>                 "Dengan jabatannya tersebut diduga RAT (Rafael Alun) menerima                 gratifikasi dari beberapa wajib pajak atas pengondisian berbagai                 temuan pemeriksaan perpajakannya," ujar Firli dalam jumpa pers                 di Gedung KPK, Jakarta Selatan, Senin (3/4/2023). Firli                 mengatakan, Rafael juga diduga memiliki beberapa usaha yang satu                 di antaranya PT Artha Mega Ekadhana (PT AME) yang bergerak dalam                 bidang jasa konsultansi terkait pembukuan dan perpajakan.`,
+    draft: false,
+    categoryId: "649b40bb-db26-4c96-81d7-d18702df459e",
+    created_at: "2023-05-02T14:19:51.974Z",
+    tags: [],
+    category: {
+      id: "649b40bb-db26-4c96-81d7-d18702df459e",
+      name: "Gaming",
+    },
+    user: {
+      id: "clh6b3v450000vng0img2thev",
+      name: "Jevon Mozart",
+      email: "jmcb1602@gmail.com",
+      emailVerified: null,
+      image:
+        "https://lh3.googleusercontent.com/a/AGNmyxbAJTBi9ghnDb9aEUJJSw9LSoI1fYn6PvkKd2Z3rA=s96-c",
+      bio: null,
+    },
+    source: [],
+    _count: {
+      likes: 0,
+      vote_up: 0,
+      vote_down: 0,
+      saved: 0,
+      comments: 0,
+    },
+  },
+  {
+    id: "7ae39b4e-599e-41e4-af7e-af9596d9f62e",
+    userId: "clh6b3v450000vng0img2thev",
+    type: "POST_SOURCE",
+    title: "Beliin anak mobil baru ",
+    body: null,
+    draft: false,
+    categoryId: "649b40bb-db26-4c96-81d7-d18702df459e",
+    created_at: "2023-05-02T14:17:07.122Z",
+    tags: [],
+    category: {
+      id: "649b40bb-db26-4c96-81d7-d18702df459e",
+      name: "Gaming",
+    },
+    user: {
+      id: "clh6b3v450000vng0img2thev",
+      name: "Jevon Mozart",
+      email: "jmcb1602@gmail.com",
+      emailVerified: null,
+      image:
+        "https://lh3.googleusercontent.com/a/AGNmyxbAJTBi9ghnDb9aEUJJSw9LSoI1fYn6PvkKd2Z3rA=s96-c",
+      bio: null,
+    },
+    source: [
+      {
+        id: "a4fde2a6-ef6b-4521-9126-cfd490120116",
+        type: "jpg",
+        url: "https://www.viv.co.id/uploads/large/b6be9ec3653241b91edd9c848d43632f.jpg",
+        threadId: "7ae39b4e-599e-41e4-af7e-af9596d9f62e",
+      },
+    ],
+    _count: {
+      likes: 0,
+      vote_up: 0,
+      vote_down: 0,
+      saved: 0,
+      comments: 0,
+    },
+  },
+];
 
 export default function Home() {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign Out</button>
-      </>
-    );
-  }
   return (
     <>
       <div className="w-full fixed top-0 z-[1] border-b py-2 px-2 lg:px-12 flex justify-between items-center gap-1 md:gap-20 bg-base-100">
         <div className="w-8 md:w-1/5">
-          <img
-            src="img/logo-combine.png"
-            className="hidden lg:w-2/4 md:block"
-          />
-          <img src="img/logo-self.png" className="w-full md:hidden" />
+          <Link href="/" title="Thred's">
+            <SingleLogo className="hidden lg:w-2/4 md:block" />
+            <FullLogo className="w-full md:hidden" />
+          </Link>
         </div>
         <div className="md:w-4/5 flex justify-between items-center gap-2">
           <div className="form-control h-full relative flex justify-center md:w-1/2">
@@ -43,30 +105,7 @@ export default function Home() {
               className="absolute right-3 lg:right-5  bg-transparent"
             />
           </div>
-          <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-              <div className="w-9 rounded-full">
-                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" />
-              </div>
-            </label>
-            <ul
-              tabIndex={0}
-              className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 w-52 border rounded"
-            >
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li>
-                <a>Logout</a>
-              </li>
-            </ul>
-          </div>
+          <UserDropdown />
         </div>
       </div>
       <div className="w-full lg:w-3/4 md:p-3 flex gap-3 mx-auto mt-16">
@@ -117,155 +156,9 @@ export default function Home() {
               />
             </Link>
           </div>
-          <div className="w-full flex">
-            <div className="w-1/12 hidden md:flex flex-col items-center text-primary">
-              <button>
-                <BiUpvote size={40} className="hidden md:block" />
-              </button>
-              <span className="font-semibold text-black">20.k</span>
-              <button>
-                <BiDownvote size={40} className="hidden md:block" />
-              </button>
-            </div>
-            <Link
-              href=""
-              className="w-full md:w-11/12 border rounded p-3 flex flex-col gap-2"
-            >
-              <div className="w-full flex gap-2">
-                <div className="avatar">
-                  <div className="w-12 rounded-full border">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4cAmdWC6aM6Q_eRDo-66c-YE5efIedF_RKLcmCfvAcg&s" />
-                  </div>
-                </div>
-                <div className="">
-                  <span className="font-semibold">Raftel Al</span>
-                  <div class="flex gap-1">
-                    <p>Dirjen***</p>
-                    <span>• 5 minutes ago</span>
-                  </div>
-                </div>
-              </div>
-              <h3 className="font-semibold">Beliin Anak Mobil Baru 😁😁❤️</h3>
-              <img
-                src="https://www.viv.co.id/uploads/large/b6be9ec3653241b91edd9c848d43632f.jpg"
-                className="w-full"
-              ></img>
-              <div className="flex justify-end gap-3">
-                <div className="md:hidden flex items-center text-primary">
-                  <button>
-                    <BiUpvote size={27} />
-                  </button>
-                  <span className="w-10 text-center font-semibold text-black">
-                    20.k
-                  </span>
-                  <button>
-                    <BiDownvote size={27} />
-                  </button>
-                </div>
-                <div className="flex items-center gap-1 font-semibold">
-                  <button>
-                    <AiOutlineHeart size={27} />
-                  </button>
-                  <p>25.4k</p>
-                </div>
-                <div className="flex items-center gap-1 font-semibold">
-                  <button>
-                    <AiOutlineComment size={27} />
-                  </button>
-                  <p>120k</p>
-                </div>
-                <div className="flex items-center gap-1 font-semibold">
-                  <button>
-                    <BsBookmark size={25} />
-                  </button>
-                  <p>5k</p>
-                </div>
-              </div>
-            </Link>
-          </div>
-          <div className="w-full flex">
-            <div className="w-1/12 hidden md:flex flex-col items-center text-primary">
-              <button>
-                <BiUpvote size={40} className="hidden md:block" />
-              </button>
-              <span className="font-semibold text-black">54.k</span>
-              <button>
-                <BiDownvote size={40} className="hidden md:block" />
-              </button>
-            </div>
-            <Link
-              href=""
-              className="w-full md:w-11/12 border rounded p-3 flex flex-col gap-2"
-            >
-              <div className="w-full flex gap-2">
-                <div className="avatar">
-                  <div className="w-12 rounded-full border">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4cAmdWC6aM6Q_eRDo-66c-YE5efIedF_RKLcmCfvAcg&s" />
-                  </div>
-                </div>
-                <div className="">
-                  <span className="font-semibold">Raftel Al</span>
-                  <div class="flex gap-1">
-                    <p>Dirjen***</p>
-                    <span>• 12 hours ago</span>
-                  </div>
-                </div>
-              </div>
-              <h3 className="font-semibold">
-                Cerpen Setelah Beliin Anak Mobil Baru 😁😁❤️
-              </h3>
-              <p className="w-full relative line-clamp-[12] before:content-[''] before:w-full before:h-20 before:absolute before:bottom-0 before:bg-gradient-to-b from-transparent to-white">
-                Sebelumnya, Mantan pejabat Direktorat Jenderal Pajak Kementerian
-                Keuangan (DJP Kemenkeu) Rafael Alun Trisambodo ditahan KPK. Dia
-                diduga menerima USD 90 ribu atau sekitar Rp 1,3 miliar melalui
-                perusahaan konsultan pajak miliknya. <br></br>
-                <br></br>Ketua KPK Firli Bahuri mengatakan, kasus ini bermula
-                saat Rafael diangkat menjadi Kepala Bidang Pemeriksaan,
-                Penyidikan, dan Penagihan Pajak pada Kantor Wilayah Ditjen Pajak
-                Jawa Timur I pada 2011.
-                <br></br>
-                <br></br>
-                "Dengan jabatannya tersebut diduga RAT (Rafael Alun) menerima
-                gratifikasi dari beberapa wajib pajak atas pengondisian berbagai
-                temuan pemeriksaan perpajakannya," ujar Firli dalam jumpa pers
-                di Gedung KPK, Jakarta Selatan, Senin (3/4/2023). Firli
-                mengatakan, Rafael juga diduga memiliki beberapa usaha yang satu
-                di antaranya PT Artha Mega Ekadhana (PT AME) yang bergerak dalam
-                bidang jasa konsultansi terkait pembukuan dan perpajakan.
-              </p>
-              <div className="flex justify-end gap-3">
-                <div className="md:hidden flex items-center text-primary">
-                  <button>
-                    <BiUpvote size={27} />
-                  </button>
-                  <span className="w-10 text-center font-semibold text-black">
-                    54.k
-                  </span>
-                  <button>
-                    <BiDownvote size={27} />
-                  </button>
-                </div>
-                <div className="flex items-center gap-1 font-semibold">
-                  <button>
-                    <AiOutlineHeart size={27} />
-                  </button>
-                  <p>2.6M</p>
-                </div>
-                <div className="flex items-center gap-1 font-semibold">
-                  <button>
-                    <AiOutlineComment size={27} />
-                  </button>
-                  <p>1.5M</p>
-                </div>
-                <div className="flex items-center gap-1 font-semibold">
-                  <button>
-                    <BsBookmark size={25} />
-                  </button>
-                  <p>200k</p>
-                </div>
-              </div>
-            </Link>
-          </div>
+          {data.map((thread) => (
+            <SingleThread thread={thread} key={thread.id} />
+          ))}
         </div>
         <div className="hidden w-2/6 lg:block">
           <div className="w-full py-6 px-4 flex flex-col items-center gap-4 border rounded mt-32">
