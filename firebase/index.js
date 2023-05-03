6e1f1d0f6a6503f6811463e455f94686962158ae
DiandraRifqiM <LuxamSky@gmail.com>
2023-05-03 11:39:42 +0700

    firebase upload file api

--- firebase/index.js
+++ firebase/index.js
@@ -1,17 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyAiXTirkZkTV-lrqk8l4f3gyJDIJmFP15g",
+  authDomain: "thred-s.firebaseapp.com",
+  projectId: "thred-s",
+  storageBucket: "thred-s.appspot.com",
+  messagingSenderId: "932176154493",
+  appId: "1:932176154493:web:a3db31447f16b6972f4614",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
