813f88e0e380206620c5e827fec9c6138e12ba54
DiandraRifqiM <LuxamSky@gmail.com>
2023-05-12 21:04:18 +0700

    like and save  feature added

---
+++ src/pages/api/thread/action/like.js
@@ -0,0 +1,39 @@
+import prisma from "../../../../../prisma/prisma";
+import { getServerAuthSession } from "../../auth/[...nextauth]";
+/**
+ *
+ * @param {import("next").NextApiRequest} req
+ * @param {import("next").NextApiResponse} res
+ * @returns
+ */
+
+export default async function handler(req, res) {
+  if (req.method != "POST")
+    return res.status(405).json({ massage: "Method not allowed" });
+
+  const session = await getServerAuthSession(req, res);
+  if (!session) return res.status(401).json({ massage: "User not authorized" });
+
+  const { id, hasLiked } = req.body;
+
+  await prisma.thread.update({
+    where: {
+      id,
+    },
+    data: {
+      likedBy: hasLiked
+        ? {
+            disconnect: {
+              id: session.user.id,
+            },
+          }
+        : {
+            connect: {
+              id: session.user.id,
+            },
+          },
+    },
+  });
+
+  res.status(201).json({ massage: "Liked success" });
+}
