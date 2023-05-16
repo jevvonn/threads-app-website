f3773e98bcc5339fa3e5322f3e9fefb15ad24e0e
FarrelBadag <mfarrelsw@gmail,com>
2023-05-16 10:56:24 +0700

    change import path

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import RecommendationSide from "@/components/user/RecommendationSide";
 import FormNav from "@/components/navigation/FormNav";
 import ThreadSkeleton from "@/components/skeleton/ThreadSkeleton";
 import { useSession } from "next-auth/react";
-import useInfiniteThreads from "@/hooks/threads/useInfiniteThreads";
+import useInfiniteThreads from "@/hooks/thread/useInfiniteThreads";
 import { useEffect } from "react";
 import useScrollPosition from "@/hooks/useScrollPosition";
 
