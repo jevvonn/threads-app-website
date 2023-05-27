dc6c9fb325c12214d0ec617fe15303401517772b
jevvonn <darmawanb985@gmail.com>
2023-05-27 15:12:09 +0700

    change ui user page

--- src/pages/u/[id].js
+++ src/pages/u/[id].js
@@ -1,166 +1,174 @@
 import FilterWidget from "@/components/navigation/FilterWidget";
 import Navbar from "@/components/navigation/Navbar";
+import ThreadSkeleton from "@/components/skeleton/ThreadSkeleton";
 import SingleThread from "@/components/thread/SingleThread";
-import { useState } from "react";
+import useInfiniteThreads from "@/hooks/thread/useInfiniteThreads";
+import useScrollPosition from "@/hooks/useScrollPosition";
+import useSingleUser from "@/hooks/user/useSingleUser";
+import { useQueryClient } from "@tanstack/react-query";
+import { useSession } from "next-auth/react";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { MdDone } from "react-icons/md";
 
-const thread = {
-  id: "clhsqqdj30004vn1oj3x97azr",
-  userId: "clhkq6kxq0000mg08fx94wa5n",
-  type: "POST_BODY",
-  title: "Mang eakk ??",
-  body: "<p>.....</p>",
-  createdAt: "2023-05-18T06:18:19.263Z",
-  updatedAt: "2023-05-18T06:18:19.263Z",
-  likedBy: [
-    {
-      id: "clhf04djz0000vn78ndb6d9kr",
-    },
-    {
-      id: "clhgco5kn0000v45cdf6pid1j",
-    },
-  ],
-  savedBy: [
-    {
-      id: "clhf04djz0000vn78ndb6d9kr",
-    },
-  ],
-  votedDownBy: [],
-  votedUpBy: [
-    {
-      id: "clhf04djz0000vn78ndb6d9kr",
-    },
-    {
-      id: "clhgco5kn0000v45cdf6pid1j",
-    },
-    {
-      id: "clhkq6kxq0000mg08fx94wa5n",
-    },
-  ],
-  tags: [
-    {
-      name: "ada aja",
-    },
-    {
-      name: "baru aja",
-    },
-    {
-      name: "buat baru",
-    },
-    {
-      name: "greeting",
-    },
-  ],
-  user: {
-    id: "clhkq6kxq0000mg08fx94wa5n",
-    name: "Annon",
-    email: "darmawanb985@gmail.com",
-    emailVerified: null,
-    image:
-      "https://lh3.googleusercontent.com/a/AGNmyxaPQIEzB9dVnH_H8GL34BaBeEQFsoVF50mcYU3h=s96-c",
-    bio: "siapa aja boleh bro",
-  },
-  sources: [],
-  _count: {
-    comments: 0,
-    likedBy: 2,
-    savedBy: 1,
-    votedDownBy: 0,
-    votedUpBy: 3,
-  },
-};
-
 export default function User() {
-  const [followed, setFollow] = useState(false);
+  const router = useRouter();
+  const { id } = router.query;
+  const { user, isLoading } = useSingleUser(id);
+  const { data: session } = useSession();
+
+  const queryKey = ["threads", { userId: id }];
+  const queryClient = useQueryClient();
+
+  const [filter, setFilter] = useState("");
+  const scrollPosition = useScrollPosition();
+
+  const { threads, isFetching, fetchNextPage, hasNextPage } =
+    useInfiniteThreads(queryKey, filter, "", id);
+
+  const handleClickFilter = (filterName) => {
+    queryClient.removeQueries({ queryKey });
+    setFilter(filterName);
+  };
+
+  useEffect(() => {
+    queryClient.refetchQueries({ queryKey });
+  }, [filter]);
+
+  useEffect(() => {
+    if (scrollPosition > 90 && hasNextPage && !isFetching) {
+      fetchNextPage();
+    }
+  }, [scrollPosition, hasNextPage, isFetching, fetchNextPage]);
 
   return (
     <>
+      <Head>
+        <title>{user?.name ? user.name : "Thred'er"}</title>
+      </Head>
+
       <Navbar />
-      <div className="w-full lg:w-3/4 py-3 flex gap-3 mx-auto mt-16">
-        <div className="lg:w-8/12 w-full md:px-3 lg:px-0 flex flex-col items-end gap-3">
-          <div className="w-full md:w-11/12 px-3 md:px-0 lg:h-32 flex gap-4 lg:items-center">
-            <div className="w-24">
-              <div className="avatar">
-                <div className="w-24 rounded-full ring-2 ring-white">
-                  <img
-                    src="https://4kwallpapers.com/images/wallpapers/saitama-one-punch-man-2560x2560-10084.jpg"
-                    alt="user-image"
-                    width={500}
-                    height={500}
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="w-full flex flex-col gap-1 lg:gap-3">
-              <span className="font-semibold text-xl">Saitama Kun</span>
-              <div className="lg:hidden flex gap-2">
-                <div className=" flex gap-2">
-                  <span className="font-semibold">17</span>
-                  <p>Following</p>
-                </div>
-                <div className="flex gap-2">
-                  <span className="font-semibold">5</span>
-                  <p>Follower</p>
-                </div>
-              </div>
-              {followed ? (
-                <button
-                  onClick={() => setFollow(false)}
-                  className="md:w-1/2 border border-primary rounded text-primary flex justify-center items-center gap-1 text-lg transition"
-                >
-                  <MdDone />
-                  <span>Following</span>
-                </button>
-              ) : (
-                <button
-                  onClick={() => setFollow(true)}
-                  className="md:w-1/2 bg-primary rounded text-white flex justify-center items-center gap-1 text-lg transition"
-                >
-                  <span className="text-2xl font-medium">+</span>Follow
-                </button>
-              )}
-            </div>
-            <div className="hidden lg:flex divider divider-horizontal mx-0 items-end" />
-          </div>
-          <div className="flex flex-col w-full border-opacity-50"></div>
-          <FilterWidget />
-          <SingleThread thread={thread} />
-        </div>
-        <div className="hidden w-4/12 lg:flex flex-col gap-6">
-          <div className="h-32 flex justify-center items-center font-semibold">
-            <div className="w-2/6 flex flex-col items-center text-lg">
-              <span>5</span>
-              <p>Follower</p>
-            </div>
-            <div className="w-2/6 flex flex-col items-center text-lg">
-              <span>17</span>
-              <p>Following</p>
-            </div>
-          </div>
-          <div className="w-full hidden border rounded lg:flex flex-col pb-10">
-            <h2 className="w-full py-2 flex justify-center items-center bg-primary rounded-t font-semibold text-xl text-white">
-              About Account
-            </h2>
-            <div className="px-4 py-5 flex flex-col gap-4">
-              <div className="flex items-center gap-2">
-                <div className="avatar">
-                  <div className="w-12 rounded-full border">
-                    <img
-                      src="https://4kwallpapers.com/images/wallpapers/saitama-one-punch-man-2560x2560-10084.jpg"
-                      alt="user-image"
-                      width={500}
-                      height={500}
+      {user && (
+        <div className="w-full lg:w-3/4 py-3 flex gap-3 mx-auto mt-16">
+          <div className="lg:w-8/12 w-full md:px-3 lg:px-0 flex flex-col items-end gap-3">
+            <div className="w-full md:w-11/12 lg:h-32 flex gap-4 lg:items-center border rounded p-2">
+              <div>
+                <div className="avatar w-16">
+                  <div className="w-16 rounded-full border">
+                    <Image
+                      src={user.image}
+                      width={50}
+                      height={50}
+                      alt={user.name}
                     />
                   </div>
                 </div>
-                <span className="font-semibold">Saitama Kun</span>
               </div>
-              <p>Botak Person</p>
-              <hr />
+              <div className="w-full flex flex-col gap-3 lg:gap-3">
+                <div>
+                  <span className="font-semibold text-xl">{user.name}</span>
+                  <div className="lg:hidden flex gap-2">
+                    <div className=" flex gap-2">
+                      <span className="font-semibold">
+                        {user._count.followedBy}
+                      </span>
+                      <p>Following</p>
+                    </div>
+                    <div className="flex gap-2">
+                      <span className="font-semibold">
+                        {user._count.follows}
+                      </span>
+                      <p>Follower</p>
+                    </div>
+                  </div>
+                </div>
+                {user.id !== session.user.id ? (
+                  user.followedBy?.length ? (
+                    <button
+                      onClick={() => setFollow(false)}
+                      className="md:w-1/2 border border-primary rounded text-primary flex justify-center items-center gap-1 text-lg transition"
+                    >
+                      <MdDone />
+                      <span>Following</span>
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => setFollow(true)}
+                      className="md:w-1/2 bg-primary rounded text-white flex justify-center items-center gap-1 text-lg transition"
+                    >
+                      <span className="text-2xl font-medium">+</span>Follow
+                    </button>
+                  )
+                ) : (
+                  <Link
+                    href={"/profile-setting"}
+                    className="md:w-1/2 border border-primary rounded text-primary flex justify-center items-center gap-1 text-lg transition"
+                  >
+                    Profile Setting
+                  </Link>
+                )}
+              </div>
+              {/* <div className="hidden lg:flex divider divider-horizontal mx-0 items-end" /> */}
+            </div>
+            <FilterWidget onFilter={handleClickFilter} />
+            {threads?.map((thread) => (
+              <SingleThread thread={thread} needToCut={true} key={thread.id} />
+            ))}
+            {isFetching && <ThreadSkeleton total={5} />}
+          </div>
+          <div className="hidden w-4/12 lg:flex flex-col gap-6">
+            <div className="h-32 flex justify-center items-center font-semibold border rounded">
+              <div className="w-2/6 flex flex-col items-center text-lg">
+                <span>{user._count.followedBy}</span>
+                <p>Follower</p>
+              </div>
+              <div className="w-2/6 flex flex-col items-center text-lg">
+                <span>{user._count.follows}</span>
+                <p>Following</p>
+              </div>
+            </div>
+            <div className="w-full hidden border rounded lg:flex flex-col">
+              <h2 className="w-full py-2 flex justify-center items-center bg-primary rounded-t font-semibold text-xl text-white">
+                About Account
+              </h2>
+              <div className="px-4 py-5 flex flex-col gap-4">
+                <div className="flex items-center gap-2">
+                  <div className="avatar">
+                    <div className="w-12 rounded-full border">
+                      <Image
+                        src={user.image}
+                        width={50}
+                        height={50}
+                        alt={user.name}
+                        className="rounded-full"
+                      />
+                    </div>
+                  </div>
+                  <span className="font-semibold">{user.name}</span>
+                </div>
+                {user.bio ? (
+                  <p>{user.bio}</p>
+                ) : (
+                  <p className="text-slate-500 italic">
+                    {user.name} doesn't have a bio.
+                  </p>
+                )}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
+      {user === null && (
+        <div className="w-full flex justify-center flex-col items-center gap-3 mt-20">
+          <h3 className="font-bold text-lg">User cannot be found.</h3>
+          <Link href={"/"} className="underline font-semibold text-blue-500">
+            Go Back To Home
+          </Link>
+        </div>
+      )}
     </>
   );
 }
